6253eea65ad5413377ed5cf8523b77fd0f08f165
Jeff Bryner <jeff@Jeffs-MacBook-Pro.local>
2020-08-07 14:49:05 -0700

    update names

--- generate_lambda_zip.py
+++ generate_lambda_zip.py
@@ -3,22 +3,22 @@ import docker
 from os import path
 
 
-def build_processor_image():
+def build_lambda_image():
     docker_client = docker.from_env()
-    docker_client.images.build(path="firehose-processor/", tag="processor", quiet=False)
+    docker_client.images.build(path="lambdas/", tag="datalake-lambdas", quiet=False)
 
 
-def get_processor_zip():
+def get_lambda_zip():
     docker_client = docker.from_env()
     docker_client.containers.run(
-        "processor",
-        "cp /asset-output/lambda.zip /mnt/cdk-data-lake/firehose-processor",
+        "datalake-lambdas",
+        "cp /asset-output/lambda.zip /mnt/cdk-data-lake/lambdas",
         volumes={path.abspath("."): {"bind": "/mnt/cdk-data-lake", "mode": "rw",}},
     )
 
 
 if __name__ == "__main__":
     print("Building image with requirements.txt")
-    build_processor_image()
+    build_lambda_image()
     print("Retrieving zip file for lambda")
-    get_processor_zip()
+    get_lambda_zip()
