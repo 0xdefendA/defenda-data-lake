8e21c0f4f503dac4aa013b566c5a951f567a1b8a
Jeff Bryner <jeff@Jeffs-MacBook-Pro.local>
2020-08-08 11:49:37 -0700

    non-naive datetime for correct isoformat

--- lambdas/normalization_plugins/event_shell.py
+++ lambdas/normalization_plugins/event_shell.py
@@ -1,37 +1,37 @@
 from utils.dict_helpers import enum_keys, merge
-from datetime import datetime
+from utils.dates import utcnow
+
 
 class message(object):
-
     def __init__(self):
-        '''
+        """
         takes an incoming message
         and ensures it matches our event shell structure
-        '''
+        """
 
-        self.registration = ['*']
+        self.registration = ["*"]
         self.priority = 2
 
     def onMessage(self, message, metadata):
         # our target shell
-        event_shell={
-            "utctimestamp": datetime.utcnow().isoformat(),
+        event_shell = {
+            "utctimestamp": utcnow().isoformat(),
             "severity": "INFO",
             "summary": "UNKNOWN",
             "category": "UNKNOWN",
             "source": "UNKNOWN",
             "tags": [],
-            "plugins":[],
-            "details": {}
+            "plugins": [],
+            "details": {},
         }
         # maybe the shell elements are already there?
-        event_set=set(enum_keys(event_shell))
-        message_set=set(enum_keys(message))
+        event_set = set(enum_keys(event_shell))
+        message_set = set(enum_keys(message))
         if not event_set.issubset(message_set):
             # we have work to do
             # merge the dicts letting any message values win
             # if the message lacks any keys, our shell values win
-            message=merge(event_shell,message)
+            message = merge(event_shell, message)
 
         # move any non shell keys to 'details'
         for item in message_set:
@@ -39,7 +39,7 @@ class message(object):
             # so check if the key is note a core element
             # present in the top level and move it to details
             if item not in event_shell and item in message:
-                message['details'][item]=message.get(item)
+                message["details"][item] = message.get(item)
                 del message[item]
 
         return (message, metadata)
