0f4ba53fcf12980791744274a43a2f95fb8d4d28
Jeff Bryner <jeff@Jeffs-MacBook-Pro.local>
2020-08-15 16:17:09 -0700

    auto remove the build container

--- generate_lambda_zip.py
+++ generate_lambda_zip.py
@@ -14,6 +14,7 @@ def get_lambda_zip():
         "datalake-lambdas",
         "cp /asset-output/lambda.zip /mnt/cdk-data-lake/lambdas",
         volumes={path.abspath("."): {"bind": "/mnt/cdk-data-lake", "mode": "rw",}},
+        auto_remove=True,
     )
 
 
