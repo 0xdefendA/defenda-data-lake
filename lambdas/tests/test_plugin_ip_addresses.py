93a11b23437a47b8d7f2e3d1b8d204d5bf4bfccd
Jeff Bryner <jeff@Jeffs-MacBook-Pro.local>
2020-09-04 07:39:29 -0700

    moar tests

--- lambdas/tests/test_plugin_ip_addresses.py
+++ lambdas/tests/test_plugin_ip_addresses.py
@@ -30,6 +30,8 @@ class TestPluginIpAddresses(object):
             "./lambdas/tests/samples/sample_cloudfront_wordpress_probe.json", "r"
         ) as f:
             self.inbound_events.append(json.loads(f.read()))
+        with open("./lambdas/tests/samples/sample_vpc_flow_log.json", "r") as f:
+            self.inbound_events.append(json.loads(f.read()))
         # run the event through default plugins
         # to set the shell and lowercase all keys
         from normalization_plugins.event_shell import message as event_shell
@@ -43,12 +45,18 @@ class TestPluginIpAddresses(object):
 
     def test_nochange(self):
         metadata = {"something": "else"}
-        # use a native raw event
-        event = self.inbound_events[0]
+        event = {}
+        # use an event without an ip
+        # to test if the plugin is benign when it should not act
+        with open("./lambdas/tests/samples/sample_syslog_sudo.json", "r") as f:
+            event = json.loads(f.read())
+        # make sure we have a valid, populated dict
+        assert len(event.keys())
         result, metadata = self.plugin.onMessage(event, metadata)
         # in = out - plugin didn't modify it
         # since it doesn't match the normalized format
         # and won't find an ip field under 'details'
+
         assert result == event
 
     def test_structure(self):
@@ -64,6 +72,11 @@ class TestPluginIpAddresses(object):
             assert "tags" in result
             assert "plugins" in result
             assert "details" in result
+            # we should have a source or destination for these events
+            assert (
+                "sourceipaddress" in result["details"]
+                or "destinationipaddress" in result["details"]
+            )
 
     def test_values(self):
         metadata = {"something": "else"}
@@ -78,3 +91,9 @@ class TestPluginIpAddresses(object):
         result, metadata = self.plugin.onMessage(event, metadata)
         logger.info(result)
         assert result["details"]["sourceipaddress"] == "139.59.66.23"
+
+        event = self.normalized_events[2]
+        result, metadata = self.plugin.onMessage(event, metadata)
+        logger.info(result)
+        assert result["details"]["sourceipaddress"] == "198.51.100.1"
+        assert result["details"]["destinationipaddress"] == "192.0.2.1"
